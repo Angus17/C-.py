7f87363e6f17192d61bda6cdc98f042c49bf7106
Diego <dileo312@hotmail.comclear>
2023-09-23 17:24:02 -0600

    Menu Tienda de Abarrotes (TERMINADO)

--- Abarrotes(Estructura).py
+++ Abarrotes(Estructura).py
@@ -41,7 +41,7 @@ def capturar_ventas():
                         os.system("cls")
                         lista[i]["IVA"] = 0.16
                         lista[i]["Subtotal"] = subtotal
-                        lista[i]["PRECIO_TOTAL (IVA incluido)"] = subtotal + (lista[i]["Subtotal"] * lista[i]["IVA"])
+                        lista[i]["PRECIO_TOTAL (IVA incluido)"] = round(subtotal + (lista[i]["Subtotal"] * lista[i]["IVA"]), 2)
                         while 1:
                             existencia_clientes = input("Existen mas clientes?\nEscribe: Si/No\n: ")
                             if existencia_clientes.lower() == "si":
@@ -66,53 +66,96 @@ def capturar_ventas():
             print("Debes ingresar un Valor adecuado")
         except TypeError:
             print("Debes ingresar un Tipo de dato correcto")
-        print(lista)
-        os.system("pause")
-        os.system("cls")
+    os.system("pause")
+    os.system("cls")
+    return lista
 
 
-def buscar_recibo_compras(Lista_total):
+def buscar_recibo_compras(lista_total):
+    bandera = False
+    
     while 1:
-        buscar = int(input("Ingresa el numero de folio del recibo: "))
-        for x in range(len(lista_total)):
-            for y in lista_total.keys("Folio"):
-                if buscar in lista_total[x][y]:
-                    print(lista_total[x][y])
+        try:
+            buscar = int(input("Ingresa el numero de folio del recibo: "))
+            for x in range(len(lista_total)):
+                if buscar == lista_total[x]["Folio"]:
+                    bandera = True
+                    print("Recibo encontrado!. . .\n")
+                    os.system("pause")
+                    print(lista_total[x])
                     break
-'''
+            if bandera:
+                break
+            else:
+                print("No existe ningun recibo con el presente folio. . .")
+        except ValueError:
+            print("Ingresa un dato correcto por favor. . .")
+        except TypeError:
+            print("Verifica que los parametros sean compatibles. . .")
+            exit()
 
-def realizar_corte():
+def realizar_corte(lista_total):
+    suma = 0
+    print(f"Se recibio un total de {len(lista_total)} clientes!\n")
+    for x in range(len(lista_total)):
+        suma += lista_total[x]["PRECIO_TOTAL (IVA incluido)"]
+    print(f"La tienda recaudo un total de ${suma} ")
 
-def mostrar_historial():
-'''        
+
+def mostrar_historial(lista_total):
+    for x in lista_total:
+        print(f"{x}\n")
 
 
 
 import os
 
+lista = []
+
 while 1:
-    while 1:
-        
-        opcion = int(input("*****MENU DE ABARROTES*****\n1.Captuar ventas "
-                        "\n2.Buscar recibo de compra\n3.Reailizar corte de caja\n"
-                        "4.Mostrar historial de ventas\n5.Salir\nSelecciona una opcion: "))
-        if 1 <= opcion <= 5:
-            break
-        else:
-            os.system("cls") 
-    match opcion:
-        case 1:
-            capturar_ventas()
-        case 2:
-            if len(lista) != 0:
-                buscar_recibo_compras(lista)
+    try:
+        while 1:
+            opcion = int(input("*****MENU DE ABARROTES*****\n1.Captuar ventas "
+                            "\n2.Buscar recibo de compra\n3.Reailizar corte de caja\n"
+                            "4.Mostrar historial de ventas\n5.Salir\nSelecciona una opcion: "))
+            if 1 <= opcion <= 5:
+                break
             else:
-                print("Verifica la disponibilidad de clientes. . .")
-            '''
+                print("Ingresa una opcion correcta. . .")
+                os.system("pause")
+                os.system("cls") 
+        match opcion:
+            case 1:
+                os.system('cls')
+                lista = capturar_ventas()
+            case 2:
+                if len(lista) != 0:
+                    buscar_recibo_compras(lista)
+                else:
+                    os.system('cls')
+                    print("Verifica la disponibilidad de clientes. . .")
+                    os.system("pause")
+                    
             case 3:
+                if len(lista) != 0:
+                    realizar_corte(lista)
+                else:
+                    os.system('cls')
+                    print("Verifica la disponibilidad de clientes. . .")
+                    os.system("pause")
+                    
                 
             case 4:
-                
-            '''
-        case 5:
-            break
+                if len(lista) != 0:
+                    mostrar_historial(lista)
+                else:
+                    os.system('cls')
+                    print("Verifica la disponibilidad de clientes. . .")
+                    os.system("pause")
+            case 5:
+                break
+    except ValueError:
+        os.system("cls")
+        print("Ingresa un dato valido y sin ausencia de valor. . .")
+        os.system("pause")
+        os.system("cls")
